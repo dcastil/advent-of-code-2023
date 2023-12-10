c27668d57b9fa9ee534c49dd4d69c121fdd459a0
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2023-12-10 15:39:07 +0100

    submit day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -18,7 +18,31 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut cards_sum = 0;
+    let mut card_copies: Vec<CardCopy> = Vec::new();
+
+    for line in input.lines() {
+        let amount_of_wins = parse_amount_of_wins(line);
+        let amount_of_current_card: u32 = 1 + card_copies
+            .iter()
+            .fold(0, |sum, current| sum + current.amount);
+
+        cards_sum += amount_of_current_card;
+
+        card_copies.retain_mut(|card_copy| {
+            card_copy.remaining_cards -= 1;
+            card_copy.remaining_cards != 0
+        });
+
+        if amount_of_wins != 0 {
+            card_copies.push(CardCopy {
+                amount: amount_of_current_card,
+                remaining_cards: amount_of_wins,
+            });
+        }
+    }
+
+    Some(cards_sum)
 }
 
 fn parse_amount_of_wins(line: &str) -> u32 {
@@ -37,6 +61,11 @@ fn parse_amount_of_wins(line: &str) -> u32 {
     amount_of_wins as u32
 }
 
+struct CardCopy {
+    amount: u32,
+    remaining_cards: u32,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,6 +79,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
