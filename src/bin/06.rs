8b083ed1e4ecf4caa8dfec6c4a1fd7297c61be61
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2023-12-12 23:13:49 +0100

    submit day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,39 +2,41 @@ use regex::Regex;
 
 advent_of_code::solution!(6);
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let races = parse_races(input);
 
-    let product_of_possible_wins: u32 = races.iter().map(get_amount_of_wins).product();
+    let product_of_possible_wins: u64 = races.iter().map(get_amount_of_wins).product();
 
     Some(product_of_possible_wins)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let race = parse_race(input);
+
+    Some(get_amount_of_wins(&race))
 }
 
-fn parse_races(input: &str) -> Vec<(u32, u32)> {
+fn parse_races(input: &str) -> Vec<(u64, u64)> {
     let space_regex = Regex::new(r"\s+").unwrap();
     let mut lines = input.lines();
     let times_string = lines.next().unwrap().split(':').nth(1).unwrap().trim();
-    let distanecs_string = lines.next().unwrap().split(':').nth(1).unwrap().trim();
+    let distances_string = lines.next().unwrap().split(':').nth(1).unwrap().trim();
 
     let times_split = space_regex.split(times_string);
-    let distances_split = space_regex.split(distanecs_string);
+    let distances_split = space_regex.split(distances_string);
 
     times_split
         .zip(distances_split)
         .map(|(time, distance)| {
-            let time = time.parse::<u32>().unwrap();
-            let record_distance = distance.parse::<u32>().unwrap();
+            let time = time.parse::<u64>().unwrap();
+            let record_distance = distance.parse::<u64>().unwrap();
 
             (time, record_distance)
         })
         .collect()
 }
 
-fn get_amount_of_wins((time_ref, record_distance_ref): &(u32, u32)) -> u32 {
+fn get_amount_of_wins((time_ref, record_distance_ref): &(u64, u64)) -> u64 {
     let time = *time_ref;
     let record_distance = *record_distance_ref;
 
@@ -57,6 +59,26 @@ fn get_amount_of_wins((time_ref, record_distance_ref): &(u32, u32)) -> u32 {
     amount_of_wins
 }
 
+fn parse_race(input: &str) -> (u64, u64) {
+    let space_regex = Regex::new(r"\s+").unwrap();
+    let mut lines = input.lines();
+    let time_string = lines.next().unwrap().split(':').nth(1).unwrap().trim();
+    let distance_string = lines.next().unwrap().split(':').nth(1).unwrap().trim();
+
+    let time = space_regex
+        .split(time_string)
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+    let distance = space_regex
+        .split(distance_string)
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+
+    (time, distance)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -70,6 +92,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(71503));
     }
 }
