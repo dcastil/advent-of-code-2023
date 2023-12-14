b2c8d353ef5af085ca2b527f6d4c282542b77ad9
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2023-12-14 22:57:05 +0100

    add newlines at end of example files

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -62,13 +62,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(6));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, None);
     }
 }
