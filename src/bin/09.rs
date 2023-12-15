43d15dc608b984ebeb73e3f18fde8747b78e0e6d
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2023-12-15 22:52:31 +0100

    submit day 9 part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -4,23 +4,7 @@ pub fn part_one(input: &str) -> Option<i32> {
     let mut sum = 0;
 
     for line in input.lines() {
-        let mut levels = Vec::new();
-
-        let mut current_values = parse_values(line);
-
-        while !values_are_zero(&current_values) {
-            let next_values = get_derivatives(&current_values);
-
-            levels.push(current_values);
-
-            current_values = next_values;
-        }
-
-        let mut interpolated_value = 0;
-
-        for values in levels.iter().rev() {
-            interpolated_value += values.last().unwrap()
-        }
+        let interpolated_value = interpolate_sequence(line, Direction::Forward);
 
         sum += interpolated_value;
     }
@@ -28,8 +12,46 @@ pub fn part_one(input: &str) -> Option<i32> {
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let interpolated_value = interpolate_sequence(line, Direction::Backward);
+
+        sum += interpolated_value;
+    }
+
+    Some(sum)
+}
+
+enum Direction {
+    Forward,
+    Backward,
+}
+
+fn interpolate_sequence(line: &str, direction: Direction) -> i32 {
+    let mut levels = Vec::new();
+
+    let mut current_values = parse_values(line);
+
+    while !values_are_zero(&current_values) {
+        let next_values = get_derivatives(&current_values);
+
+        levels.push(current_values);
+
+        current_values = next_values;
+    }
+
+    let mut interpolated_value = 0;
+
+    for values in levels.iter().rev() {
+        interpolated_value = match direction {
+            Direction::Forward => values.last().unwrap() + interpolated_value,
+            Direction::Backward => values.first().unwrap() - interpolated_value,
+        }
+    }
+
+    interpolated_value
 }
 
 fn parse_values(line: &str) -> Vec<i32> {
@@ -61,6 +83,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
