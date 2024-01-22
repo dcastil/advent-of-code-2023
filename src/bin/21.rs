44687c91a961a534ec94787330354a8fa176b61e
Dany Castillo <31006608+dcastil@users.noreply.github.com>
2024-01-23 00:16:51 +0100

    submit day 21 part 2

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,19 +1,49 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(21);
 
 pub fn part_one(input: &str) -> Option<usize> {
-    part_one_with_step_count(input, 64)
+    part_one_with_step_count(input, 65)
 }
 
 fn part_one_with_step_count(input: &str, step_count: u8) -> Option<usize> {
     let garden = Garden::from_input(input);
+    let positions = garden.get_possible_positions(step_count);
 
-    Some(garden.get_possible_positions(step_count).len())
+    Some(positions.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+// Solution from https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21
+pub fn part_two(input: &str) -> Option<usize> {
+    let garden = Garden::from_input(input);
+    let distance_map = garden.get_distance_map();
+
+    let even_positions_corners_count = distance_map
+        .values()
+        .filter(|&&v| v % 2 == 0 && v > 65)
+        .count();
+    let odd_positions_corners_count = distance_map
+        .values()
+        .filter(|&&v| v % 2 == 1 && v > 65)
+        .count();
+    let even_positions_full_count = distance_map.values().filter(|&&v| v % 2 == 0).count();
+    let odd_positions_full_count = distance_map.values().filter(|&&v| v % 2 == 1).count();
+
+    // We know that the map has the same length in x and y direction
+    let map_length = (garden.max_x + 1) as usize;
+    let n = (26501365 - map_length / 2) / map_length;
+
+    assert_eq!(n, 202300);
+
+    let odd_tiles_count = (n + 1).pow(2);
+    let even_tiles_count = n.pow(2);
+
+    let positions_count = odd_tiles_count * odd_positions_full_count
+        + even_tiles_count * even_positions_full_count
+        - (n + 1) * odd_positions_corners_count
+        + n * even_positions_corners_count;
+
+    Some(positions_count)
 }
 
 struct Garden {
@@ -32,18 +62,19 @@ impl Garden {
 
         for (y, line) in input.lines().enumerate() {
             for (x, character) in line.as_bytes().iter().enumerate() {
-                let coordinate = Coordinate {
-                    x: x as u8,
-                    y: y as u8,
-                };
-
                 match character {
-                    b'#' => {
-                        obstacle_map.insert(coordinate);
-                    }
                     b'.' => (),
+                    b'#' => {
+                        obstacle_map.insert(Coordinate {
+                            x: x as u8,
+                            y: y as u8,
+                        });
+                    }
                     b'S' => {
-                        start = Some(coordinate);
+                        start = Some(Coordinate {
+                            x: x as u8,
+                            y: y as u8,
+                        });
                     }
                     _ => panic!("Unknown character: {}", character),
                 }
@@ -68,10 +99,10 @@ impl Garden {
         let mut positions = HashSet::from([self.start.clone()]);
 
         for _ in 0..step_count {
-            let mut new_positions = HashSet::new();
+            let mut next_positions = HashSet::new();
 
             for coordinate in positions {
-                new_positions.extend(
+                next_positions.extend(
                     [
                         Direction::Up,
                         Direction::Down,
@@ -83,10 +114,43 @@ impl Garden {
                 );
             }
 
-            positions = new_positions;
+            positions = next_positions;
         }
 
         positions
+    }
+
+    fn get_distance_map(&self) -> HashMap<Coordinate, u8> {
+        let mut distance_map = HashMap::new();
+
+        let mut edge_positions = vec![self.start.clone()];
+        let mut distance = 0;
+
+        while !edge_positions.is_empty() {
+            let mut next_edge_positions = Vec::new();
+
+            for coordinate in edge_positions {
+                distance_map.entry(coordinate.clone()).or_insert_with(|| {
+                    next_edge_positions.extend(
+                        [
+                            Direction::Up,
+                            Direction::Down,
+                            Direction::Left,
+                            Direction::Right,
+                        ]
+                        .iter()
+                        .filter_map(|direction| self.get_next_coordinate(&coordinate, direction)),
+                    );
+
+                    distance
+                });
+            }
+
+            edge_positions = next_edge_positions;
+            distance += 1;
+        }
+
+        distance_map
     }
 
     fn get_next_coordinate(
@@ -160,11 +224,5 @@ mod tests {
         let result =
             part_one_with_step_count(&advent_of_code::template::read_file("examples", DAY), 6);
         assert_eq!(result, Some(16));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
     }
 }
